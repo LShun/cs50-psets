7e3cc163b922815925e433cec6834cab02024bfe
Hiew L. Shun <LShun@users.noreply.github.com>
2018-04-28 10:53:04 +0800

    Add files via upload

---
+++ 2018/week-1/mario.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int h, s;
+    do
+    {
+        h = get_int("Height: "); //asks the user for the pyramid height.
+    }
+    while (h < 0 || h > 23);
+    //Loop through the number of rows of pyramid based on h
+    for (int i = 0; i < h; i++)
+    {
+        //For 'x' row, print out 'h-x' spaces followed by 'x' hashes.
+        for (s = 1; s < h - i; s++)
+        {
+            printf(" ");
+        }
+        for (int hash = 0; hash <= h - s; hash++)
+        {
+            printf("#");
+        }
+        //Then insert two spaces and print 'x' hashes
+        printf("  ");
+        for (int aftspahash = 0; aftspahash <= i; aftspahash++)
+        {
+            printf("#");
+        }
+        //At the end, print a new line.
+        printf("\n");
+    }
+
+
+}
